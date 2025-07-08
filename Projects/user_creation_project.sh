4649aad621f8f75e0b0d76c08aa345f0d68ea0ee
Arijit Nandy <98833269+Arijit1610@users.noreply.github.com>
2025-07-08 08:24:57 +0000

    added usercreation project

--- Projects/user_creation_project.sh
+++ Projects/user_creation_project.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+#script should be execute with sudo/root access
 if [[ "${UID}" -ne 0 ]]
 then
 	echo 'Please run with sudo or root. '
 	exit 1
 fi
+#user should be provide atlest one argument as username else guide him 
+
 if [[ "${#}" -lt 1 ]]
 then
 	echo "Usage ${0} USER_NAME [COMMENT]..."
@@ -11,3 +15,48 @@ then
 	exit 1
 fi
 
+#store 1st argument as username
+userName=$1
+
+echo "$userName"
+
+
+#In case of more then one argument store it as account comments
+
+shift
+comment=$@
+#echo "$comment"
+
+#create password
+PASSWORD=$(date +%s%N)
+#echo $PASSWORD
+
+#crete the user
+useradd -c $COMMENT -m $userName 
+#check if the user successfully created or not
+if [[ $? -ne 0 ]]  #checking status of previous excuted command
+then
+	echo "The account could not be created"
+	exit 1
+fi
+
+#set the password for that user
+echo -e "$PASSWORD\n$PASSWORD" | passwd  $user_NAME
+
+# check if passowowd is successfully set or  not
+if [[ $? -ne 0 ]]  #checking status of previous excuted command
+then
+	echo "Password could not be set"
+	exit 1
+fi
+
+
+#force password change on first login
+
+passwd -e $userName
+
+#Display the username, password, and the host where created
+
+echo "Username: $userName"
+echo "Passwod: $PASSWORD"
+echo $hostname
